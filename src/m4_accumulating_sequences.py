5ab80060fa1624b8932f8ce9c93e37bee5e2cff9
mufichmj <mufichmj@rose-hulman.edu>
2018-04-14 01:07:48 -0400

    m1

--- src/m4_accumulating_sequences.py
+++ src/m4_accumulating_sequences.py
@@ -141,7 +141,7 @@ def make_simple_string(m, n):
 def run_test_make_less_simple_string():
     """ Tests the   make_less_simple_string    function. """
     # ------------------------------------------------------------------
-    # TODO: 6. Implement this TEST function.
+    # DONE: 6. Implement this TEST function.
     #   It TESTS the  make_less_simple_string  function defined below.
     #   Include at least **   2   ** tests.
     #
@@ -154,13 +154,13 @@ def run_test_make_less_simple_string():
 
     #Test 1
     expected = '5-6-7-8-9-10-11-12-13'
-    actual = make_simple_string(5, 13)
+    actual = make_less_simple_string(5, 13)
     print('Expected:', expected)
     print('Actual:  ', actual)
 
     #Test 2
     expected = '2-3-4-5-6-7-8-9'
-    actual = make_simple_string(2, 9)
+    actual = make_less_simple_string(2, 9)
     print('Expected:', expected)
     print('Actual:  ', actual)
 
@@ -187,12 +187,14 @@ def make_less_simple_string(m, n):
       :type n: int
     """
     # ------------------------------------------------------------------
-    # TODO: 7. Implement and test this function.
+    # DONE: 7. Implement and test this function.
     #   Note that you should write its TEST function first (above).
     # -----------------------------------------------------------------
     sequence = ""
-    for k in range(m, n + 1):
+    for k in range(m, n):
         sequence = sequence + str(k) + "-"
+
+    sequence = sequence + str(n)
 
     return sequence
 
