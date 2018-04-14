258add55d24a7a142712b7a364be06481231df4f
mufichmj <mufichmj@rose-hulman.edu>
2018-04-14 01:56:22 -0400

    m5

--- src/m5_more_sequences.py
+++ src/m5_more_sequences.py
@@ -76,7 +76,7 @@ def sum_radii(circles):
       :rtype: int | float
     """
     # ------------------------------------------------------------------
-    # TODO: 2. Implement and test this function.
+    # DONE 2. Implement and test this function.
     #     The testing code is already written for you (above).
     #
     # Note: No fair using "slices" on ANY of these problems,
@@ -159,7 +159,12 @@ def count_last_n_odds(integers, n):
     # TODO: 3. Implement and test this function.
     #     The testing code is already written for you (above).
     # ------------------------------------------------------------------
+    count = 0
+    for k in range(len(integers) - n, len(integers)):
+        if integers[k] % 2 == 1:
+            count = count + 1
 
+    return count
 
 # ----------------------------------------------------------------------
 # Some problems iterate (loop) through PART of the sequence,
